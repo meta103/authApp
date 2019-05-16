6ffcf290bba4cb628ada59f3e73f680274b91a5b
Marcos Tagliabue <mtagliabue@profile.local>
2019-05-16 17:14:16 +0200

    finished

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate{
+
+  constructor(private auth: AuthService) { }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    console.log(next)
+    if (this.auth.isAuthenticated()) {
+      console.log("Estas loggeado");
+      return true;
+    } else {
+      console.error("bloqueado por el guard");
+      return false;
+    }
+  }
+}
